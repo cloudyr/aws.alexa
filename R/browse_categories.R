b84c1b7f05965da5574acef798172d1cf140ebd6
Gaurav Sood <gsood07@gmail.com>
2016-05-24 17:34:46 -0400

    checking results. error tolerance

--- R/browse_categories.R
+++ R/browse_categories.R
@@ -7,7 +7,7 @@
 #' @param description Boolean; Optional; Whether or not to return descriptions of categories; Default is TRUE
 #' @param \dots Additional arguments passed to \code{\link{alexa_GET}}.
 #' 
-#' @return data.frame
+#' @return data.frame with 5 columns: path, title, sub_category_count, total_listing_count, description
 #'  
 #' @export
 #' @references \url{http://docs.aws.amazon.com/AlexaWebInfoService/latest/index.html?ApiReference_CategoryBrowseAction.html}
@@ -18,11 +18,15 @@
 browse_categories <- function(path = NULL, response_group = "Categories", description = TRUE, ...) {
     
     if (!is.character(path)) {
-        stop("Must specify valid path.")
+        stop("Please specify a valid path.")
     }
 
     if (!is.logical(description)) {
-   		stop("Provide a valid value for description.")
+   		stop("Please provide a valid value for description.")
+    }
+
+    if (!(response_group %in% c("Categories", "RelatedCategories", "LanguageCategories", "LetterBars"))) {
+        stop("Please provide a valid value for response_group.")
     }
 
     # Convert Boolean to String
@@ -32,5 +36,12 @@ browse_categories <- function(path = NULL, response_group = "Categories", descri
 
     browse_cat <- alexa_GET(query, ...)
 
-    browse_cat
-}
+    if (is.null(browse_cat[[2]][[1]][[1]][[1]])) {
+        message("No Results. It is likely that the path you provided is incorrect.")
+        return(NULL)
+    }
+
+    res <- do.call(rbind.fill, lapply(browse_cat[[2]][[1]][[1]][[1]], data.frame))
+    names(res) <- c("path", "title", "sub_category_count", "total_listing_count", "description")
+    res
+}   
