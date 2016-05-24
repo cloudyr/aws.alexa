5b407bc4f7df197aa4588c9bad63b4f1d44b13e1
Thomas J. Leeper <thosjleeper@gmail.com>
2016-05-24 15:02:52 +0100

    modify tests slightly (#2)

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,10 @@
-library(testthat)
-library(aws.alexa)
+library("testthat")
+library("aws.alexa")
 
-test_check("aws.alexa")
+token_file <- try(suppressWarnings(readLines("aws.alexa_key")), silent = TRUE)
+if (!inherits(token_file, "try-error")) {
+    key <- unlist(strsplit(token, ","))[1]
+    secret <- unlist(strsplit(token, ","))[2]
+    set_secret_key(key, secret)
+    test_check("aws.alexa")
+}
--- tests/testthat/test-get-info.R
+++ tests/testthat/test-get-info.R
@@ -1,16 +1,6 @@
 context("Get URL Info.")
 
 test_that("url_info happens successfully", {
-	
-  skip_on_cran()
-
-  token_file <- file("aws.alexa_key", "r")
-  token <- suppressWarnings(readLines(token_file))
-  close(token_file)
-  key <- unlist(strsplit(token, ","))[1]
-  secret <- unlist(strsplit(token, ","))[2]
-  set_secret_key(key, secret)
-  
-  get_info <- url_info("google.com")
-  expect_that(get_info, is_a("list"))
+    get_info <- url_info("google.com")
+    expect_that(get_info, is_a("list"))
 })
