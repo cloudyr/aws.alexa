e52cc437517ead8630682a34822c0a34f851f022
Pawel Koperek <p.koperek@eyeo.com>
2018-08-01 16:22:37 +0200

    include missing function parameters rerun roxygen2

--- R/aws.alexa.package.R
+++ R/aws.alexa.package.R
@@ -13,7 +13,7 @@
 #' And set these using \code{\link{set_secret_key}}
 #' 
 #' @importFrom stats setNames
-#' @importFrom httr GET content stop_for_status
+#' @importFrom httr GET content stop_for_status add_headers
 #' @importFrom xml2 read_xml as_list
 #' @importFrom dplyr bind_rows
 #' @importFrom aws.signature signature_v4_auth locate_credentials
@@ -31,11 +31,21 @@ NULL
 #' @param query query list 
 #' @param key A character string containing an AWS Access Key ID. The default is retrieved from \code{Sys.getenv("AWS_ACCESS_KEY_ID")}.
 #' @param secret A character string containing an AWS Secret Access Key. The default is retrieved from \code{Sys.getenv("AWS_SECRET_ACCESS_KEY")}.
+#' @param verbose A logical indicating whether to be verbose. Default is given by \code{options("verbose")}.
+#' @param session_token Optionally, a character string containing an AWS temporary Session Token. If missing, defaults to value stored in environment variable \env{AWS_SESSION_TOKEN}.
+#' @param region A character string containing the AWS region. If missing, defaults to \dQuote{us-west-1}.
+#' @param headers A list of request headers for the REST call. 
 #' @param \dots Additional arguments passed to \code{\link[httr]{GET}}.
 #' @return list
 
-alexa_GET <- function(query, key = Sys.getenv("AWS_ACCESS_KEY_ID"),
-                            secret = Sys.getenv("AWS_SECRET_ACCESS_KEY"), ...) {
+alexa_GET <- function(query, 
+                      key = Sys.getenv("AWS_ACCESS_KEY_ID"),
+                      secret = Sys.getenv("AWS_SECRET_ACCESS_KEY"), 
+                      verbose = getOption("verbose", FALSE),
+                      session_token = NULL,
+                      region = 'us-west-1',
+                      headers = list(),
+                      ...) {
 
   if (identical(key, "") | identical(secret, "")) {
     stop("Please set application id and password using set_secret_key(key='key',
@@ -81,11 +91,10 @@ alexa_GET <- function(query, key = Sys.getenv("AWS_ACCESS_KEY_ID"),
     headers[["x-amz-security-token"]] <- session_token
   }
   headers[["Authorization"]] <- Sig[["SignatureHeader"]]
-
-  res <- GET("https://awis.amazonaws.com/api",
-             add_headers(headers),
-             query = query)
-
+  H <- do.call(add_headers, headers)
+  
+  res <- GET("https://awis.amazonaws.com/api", H, query = query)
+  
   alexa_check(res)
   res <- as_list(read_xml(content(res, as = "text", encoding = "utf-8")))
 
