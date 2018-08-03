3556823b43c3f1402e74a994fb36b20e6ff5331c
Pawel Koperek <pkoperek@gmail.com>
2018-08-03 17:41:18 +0200

    Change signature v2 to v4 (#11)
    * change signature_v2 -> signature v4
    
    * bump up version
    
    * fix imports from signature package
    
    * run roxygen2 on the repo
    
    * include missing function parameters
    rerun roxygen2

--- R/aws.alexa.package.R
+++ R/aws.alexa.package.R
@@ -13,10 +13,10 @@
 #' And set these using \code{\link{set_secret_key}}
 #' 
 #' @importFrom stats setNames
-#' @importFrom httr GET content stop_for_status
+#' @importFrom httr GET content stop_for_status add_headers
 #' @importFrom xml2 read_xml as_list
 #' @importFrom dplyr bind_rows
-#' @importFrom aws.signature signature_v2_auth
+#' @importFrom aws.signature signature_v4_auth locate_credentials
 #' @docType package
 #' @author Gaurav Sood
 NULL
@@ -31,27 +31,70 @@ NULL
 #' @param query query list 
 #' @param key A character string containing an AWS Access Key ID. The default is retrieved from \code{Sys.getenv("AWS_ACCESS_KEY_ID")}.
 #' @param secret A character string containing an AWS Secret Access Key. The default is retrieved from \code{Sys.getenv("AWS_SECRET_ACCESS_KEY")}.
+#' @param verbose A logical indicating whether to be verbose. Default is given by \code{options("verbose")}.
+#' @param session_token Optionally, a character string containing an AWS temporary Session Token. If missing, defaults to value stored in environment variable \env{AWS_SESSION_TOKEN}.
+#' @param region A character string containing the AWS region. If missing, defaults to \dQuote{us-west-1}.
+#' @param headers A list of request headers for the REST call. 
 #' @param \dots Additional arguments passed to \code{\link[httr]{GET}}.
 #' @return list
 
-alexa_GET <- function(query, key = Sys.getenv("AWS_ACCESS_KEY_ID"),
-                            secret = Sys.getenv("AWS_SECRET_ACCESS_KEY"), ...) {
+alexa_GET <- function(query, 
+                      key = Sys.getenv("AWS_ACCESS_KEY_ID"),
+                      secret = Sys.getenv("AWS_SECRET_ACCESS_KEY"), 
+                      verbose = getOption("verbose", FALSE),
+                      session_token = NULL,
+                      region = 'us-west-1',
+                      headers = list(),
+                      ...) {
 
   if (identical(key, "") | identical(secret, "")) {
     stop("Please set application id and password using set_secret_key(key='key',
                                                             secret='secret')).")
   }
+  
+  # locate and validate credentials
+  credentials <- locate_credentials(key = key, 
+                                    secret = secret, 
+                                    session_token = session_token, 
+                                    region = region, 
+                                    verbose = verbose)
 
-  sig <- signature_v2_auth(datetime = format(Sys.time(),
-                           format = "%Y-%m-%dT%H:%M:%SZ", tz = "UTC"),
+  key <- credentials[["key"]]
+  secret <- credentials[["secret"]]
+  session_token <- credentials[["session_token"]]
+  region <- credentials[["region"]]
+
+  hostname <- paste0("awis.", region, ".amazonaws.com")
+  current <- Sys.time()
+  header_timestamp <- format(current, "%Y-%m-%dT%H:%M:%SZ", tz = "UTC")
+  canonical_headers <- c(list(host = hostname,
+                              `x-amz-date` = header_timestamp), headers)
+
+  # generate signatures
+  Sig <- signature_v4_auth(datetime = format(current,
+                                             "%Y%m%dT%H%M%SZ", tz = "UTC"),
+                           region = region,
+                           service = "awis",
                            verb = "GET",
-                           service = "awis.amazonaws.com",
-                           path = "/",
+                           action = "/api",
                            query_args = query,
+                           canonical_headers = canonical_headers,
+                           request_body = "",
                            key = key,
-                           secret = secret)
+                           secret = secret,
+                           session_token = session_token,
+                           verbose = verbose)
 
-  res <- GET("http://awis.amazonaws.com", query = sig$Query, ...)
+  headers[["x-amz-date"]] <- header_timestamp
+  headers[["x-amz-content-sha256"]] <- Sig$BodyHash
+  if (!is.null(session_token) && session_token != "") {
+    headers[["x-amz-security-token"]] <- session_token
+  }
+  headers[["Authorization"]] <- Sig[["SignatureHeader"]]
+  H <- do.call(add_headers, headers)
+  
+  res <- GET("https://awis.amazonaws.com/api", H, query = query)
+  
   alexa_check(res)
   res <- as_list(read_xml(content(res, as = "text", encoding = "utf-8")))
 
